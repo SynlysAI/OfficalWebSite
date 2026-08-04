23bd43012c0c7670a60fe2b384fa50ff6c41523a
fyk <342363948@qq.com>
2026-08-04 17:20:30 +0800

    配置 Vite 开发服务器支持外部访问
    - 设置 host 为 0.0.0.0，允许外部设备通过局域网访问
    - 固定开发服务器端口为 5175

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // 监听所有网络接口，允许外部电脑访问
+    port: 5175,       // 固定端口，方便外部访问
+  },
 })
